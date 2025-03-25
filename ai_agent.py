0df69e9c1619121003682a2d4ceb3b54ffb7fe15
Subrat Pandey <subratpandey974@gmail.com>
2025-03-25 16:20:12 -0500

    setup complete backend

--- ai_agent.py
+++ ai_agent.py
@@ -15,24 +15,30 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 openai_llm = ChatOpenAI(model="gpt-4o-mini")
 groq_llm = ChatGroq(model="llama-3.3-70b-versatile")
 
-search_tool = TavilySearchResults(max_results=2)
 system_prompt = '''Act as an AI chatbot who is smart and friendly. 
                 Give amazing responses to the questions asked.
                 Be thoughtful and mindful while giving your responses.'''
 
-agent = create_react_agent(
-    model = groq_llm,
-    tools = [search_tool],
-    state_modifier = system_prompt
-)
+def get_response_from_ai_agent(llm_id, query, allow_search, system_prompt, provider):
+    if provider == "Groq":
+        llm = ChatGroq(model = llm_id)
+    elif provider == "OpenAI":
+        llm = ChatOpenAI(model = llm_id)
 
-query = "tell me about recent crypto trends"
-state = {"messages": query}
-response = agent.invoke(state)
-messages = response.get("messages")
-ai_messages = [message.content for message in messages if isinstance(message, AIMessage)]
+    tools = [TavilySearchResults(max_results=2)] if allow_search else []
 
-print(ai_messages[-1])
+    agent = create_react_agent(
+        model = llm,
+        tools = tools,
+        state_modifier = system_prompt
+    )
+
+    state = {"messages": query}
+    response = agent.invoke(state)
+    messages = response.get("messages")
+    ai_messages = [message.content for message in messages if isinstance(message, AIMessage)]
+
+    return ai_messages[-1]
 
 
 
